0ed19fef147d0f699bee1a1b7fd58748259f7944
Chris Farrugia <chrisfarrugia.dev@gmail.com>
2025-07-16 21:46:43 +0200

    update progress

--- web.frontend.vue/src/views/OrganisationView.vue
+++ web.frontend.vue/src/views/OrganisationView.vue
@@ -3,14 +3,21 @@
     <section class="vview__section">
         <div class="heading--2 ">Organisations</div>
 
+            <TheFlashMessage></TheFlashMessage>
+
             <TheTabs
                 class="mt-16"
                 :tabsObjectData="tabsObjectData_1"
                 :isDisabled="false"
                 :layoutBreakpoint="500"
-                @setActiveTab="tabsObjectData_1.activeTab = $event"
+                @setActiveTab="tabsObjectData_1.activeTab = $event as TabKey"
                 @click="clearMessage"
             ></TheTabs>
+
+
+            <KeepAlive>
+                <component :is="componentMap[tabsObjectData_1.activeTab]" class="mt-6"></component>
+            </KeepAlive> 
 
     </section>
 
@@ -20,17 +27,29 @@
 <!-- --------------------------------------------------------------- -->
 
 <script setup lang="ts">
+import TheFlashMessage from '@/components/commen/TheFlashMessage.vue';
 import TheTabs from '@/components/commen/TheTabs.vue';
-import { reactive } from 'vue';
+import AddOrganisation from '@/components/organisation/AddOrganisation.vue';
+import AllOrganisations from '@/components/organisation/AllOrganisations.vue';
+import { markRaw, reactive, type Component } from 'vue';
 
 // - Data --------------------------------------------------------------
 
+// Mapping of tab identifiers to component objects for dynamic loading
+
+type TabKey = 'AllOrganisations' | 'AddOrganisation';
+
+const componentMap: Record<TabKey, Component> = {
+    AllOrganisations: markRaw(AllOrganisations),
+    AddOrganisation: markRaw(AddOrganisation),
+};
+
 // Reactive state to manage tab data and the active tab identifier
 const tabsObjectData_1 = reactive({
-    activeTab: 'OrganisationsTable',
+    activeTab: 'AllOrganisations' as TabKey,
     tabs: {
-        OrganisationsTable: 'All Organisations',
-        CreateOrganisationsForm: 'Add Organisation',    
+        AllOrganisations: 'All Organisations',
+        AddOrganisation: 'Add Organisation',    
     },
 });
 
